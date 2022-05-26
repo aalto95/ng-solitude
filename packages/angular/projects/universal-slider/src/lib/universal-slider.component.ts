be2b337920f976d01ad5c14e80fa2057d170cd17
y2k01 <scotlandfjord@protonmail.com>
2022-05-26 16:07:39 +0900

    [angular] Add gesture swipes

--- packages/angular/projects/universal-slider/src/lib/universal-slider.component.ts
+++ packages/angular/projects/universal-slider/src/lib/universal-slider.component.ts
@@ -11,10 +11,13 @@ export class UniversalSliderComponent implements OnInit {
 
   @Input()
   slides: Slide[] = [];
-
+  slideDirection: string = '';
   activeSlide: number = 0;
+  downX: number = 1;
+  upX: number = 1;
 
   slideLeft() {
+    this.slideDirection = 'left';
     if (this.activeSlide > 0) {
       this.activeSlide -= 1;
     } else {
@@ -23,11 +26,38 @@ export class UniversalSliderComponent implements OnInit {
   }
 
   slideRight() {
+    this.slideDirection = 'right';
     if (this.activeSlide < this.slides.length - 1) {
       this.activeSlide += 1;
     } else {
       this.activeSlide = 0;
     }
+  }
+
+  gesturedSlide() {
+    if (this.downX + 50 < this.upX) {
+      this.slideLeft();
+    } else if (this.downX > this.upX + 50) {
+      this.slideRight();
+    }
+  }
+
+  listenToMousedown(e: MouseEvent) {
+    this.downX = e.clientX;
+  }
+
+  listenToMouseup(e: MouseEvent) {
+    this.upX = e.clientX;
+    this.gesturedSlide();
+  }
+
+  listenToTouchstart(e: TouchEvent) {
+    this.downX = e.touches[0].clientX;
+  }
+
+  listenToTouchend(e: TouchEvent) {
+    this.upX = e.changedTouches[0].clientX;
+    this.gesturedSlide();
   }
 
   ngOnInit(): void {}
